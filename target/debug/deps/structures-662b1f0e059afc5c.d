4fb4d9d54e42cb8368c6d3a571c41469bb507378
Madaka Heri <madakaheri@gmail.com>
2023-09-18 18:33:21 +0900

    recommit

---
+++ target/debug/deps/structures-662b1f0e059afc5c.d
@@ -0,0 +1,5 @@
+/Users/madakaheri/github/rust-samples/target/debug/deps/structures-662b1f0e059afc5c.rmeta: structure/src/main.rs
+
+/Users/madakaheri/github/rust-samples/target/debug/deps/structures-662b1f0e059afc5c.d: structure/src/main.rs
+
+structure/src/main.rs:
---
+++ target/debug/deps/structures-9691608bc888053f.d
@@ -0,0 +1,5 @@
+/Users/madakaheri/github/rust-samples/target/debug/deps/structures-9691608bc888053f.rmeta: structure/src/main.rs
+
+/Users/madakaheri/github/rust-samples/target/debug/deps/structures-9691608bc888053f.d: structure/src/main.rs
+
+structure/src/main.rs:
